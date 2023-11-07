2dbcfaa33df5e510b47dedcd6451ee2ce8f0cfff
key-money <wangingimoses102@gmail.com>
2023-11-07 07:28:04 +0000

    complete version 4

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+/* script that listens for changes on input checkbox */
+$('document').ready(function () {
+  const amenitiesId = {};
+  $('INPUT[type="checkbox"]').click(function () {
+    if ($(this).prop('checked')) {
+      amenitiesId[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenitiesId[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenitiesId).join(', '));
+  });
+});
